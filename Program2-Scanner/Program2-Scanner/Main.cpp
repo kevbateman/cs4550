20e34962dcf3fa8f5369b0cd4b1e4ddd04d439ed
Kevin Bateman <kbatem13@dmail.dixie.edu>
2016-02-17 16:36:34 -0700

    finished creating symbol table and node classes

--- Program2-Scanner/Program2-Scanner/Main.cpp
+++ Program2-Scanner/Program2-Scanner/Main.cpp
@@ -1,6 +1,7 @@
 #include "Token.h"
 #include "Scanner.h"
 #include "Debug.h"
+#include "Symbol.h"
 
 #include <iostream>
 #include <iomanip>
@@ -16,6 +17,54 @@ int main() {
 		my_token = my_scanner.GetNextToken();
 		std::cout << my_scanner.GetLineNumber() << std::setw(3) << ' ' << my_token << std::endl;
 	} while (my_token.GetTokenType() != ENDFILE_TOKEN);
+
+	//SymbolTableClass table;
+	//MSG("Adding 'FIRST'")
+	//table.AddEntry("FIRST");
+
+	//MSG("Adding 'SECOND'")
+	//table.AddEntry("SECOND");
+
+	//MSG("Adding 'THIRD'")
+	//table.AddEntry("THIRD");
+
+	////MSG("ADDING 'FIRST'");
+	////table.AddEntry("FIRST");
+
+	//MSG("Checking if 'SECOND' exists:");
+	//bool second_exists = table.Exists("SECOND");
+	//if (second_exists) {
+	//	MSG("true");
+	//}
+	//else {
+	//	MSG("false");
+	//}
+
+	//MSG("Getting number of added variables:");
+	//int var_count = table.GetCount();
+	//MSG(var_count);
+	//
+	//MSG("Getting index of 'SECOND':");
+	//int second_index = table.GetIndex("SECOND");
+	//MSG(second_index);
+
+	//MSG("Getting index of 'FOURTH':");
+	//int fourth_index = table.GetIndex("FOURTH");
+	//MSG(fourth_index);
+
+	//MSG("Setting 'FIRST' value to 4");
+	//table.SetValue("FIRST", 4);
+
+	////MSG("Setting 'FOURTH' value to 1")
+	////table.SetValue("FOURTH", 1);
+
+	//MSG("Getting 'FIRST' value:");
+	//int first_value = table.GetValue("FIRST");
+	//MSG(first_value);
+
+	//MSG("Getting 'SECOND' value:");
+	//int second_value = table.GetValue("SECOND");
+	//MSG(second_value);
 
 	return 0;
 }
